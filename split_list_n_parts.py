7659e9f9ee930239da07d49bb97f3e60aa515431
Naren <naren@impythonist.in>
2015-02-26 12:33:57 +0530

    Create split_list_n_parts.py

---
+++ split_list_n_parts.py
@@ -0,0 +1,4 @@
+def split_list(alist, wanted_parts=1):
+    length = len(alist)
+    return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts] 
+             for i in range(wanted_parts) ]
