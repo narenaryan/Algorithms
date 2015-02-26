1aa293ec05bf4605d7207921c8fd790434599e8c
Naren <naren@impythonist.in>
2015-02-26 17:53:50 +0530

    Update primegen.py

--- primegen.py
+++ primegen.py
@@ -1,16 +1,20 @@
-def sieve_for_primes_to(n):
+#!/usr/bin/python
+import sys
+
+
+def gen_prime(n):
     size = n//2
-    sieve = [1]*size
-    limit = int(n**0.5)
+    nsieve,limit = [1]*size,int(n**0.5)
     for i in range(1,limit):
-        if sieve[i]:
-            val = 2*i+1
-            tmp = ((size-1) - i)//val 
-            sieve[i+val::val] = [0]*tmp
-    return sieve
-    
-def gen_prime(l):
-    print [2] + [i*2+1 for i, v in enumerate(sieve_for_primes_to(l)) if v and i>0]
-    
-    
-gen_prime(10000000)
+        if nsieve[i]:
+            gval = 2*i+1
+            tmp = ((size-1) - i)//gval 
+            nsieve[i+gval::gval] = [0]*tmp
+    return nsieve
+
+fil = sys.argv[-1]
+
+with open(fil,'r') as f:
+    arr = [i.rstrip() for i in f.readlines()]
+    for item in arr:
+        print ','.join([str(x) for x in iter([2] + [i*2+1 for i, v in enumerate(gen_prime(int(item))) if v and i>0])])
