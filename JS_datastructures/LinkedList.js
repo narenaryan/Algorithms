b324843fdfb69d34861c25d794689a3d45eb553e
naren <naren.yellavula@knowlarity.com>
2016-07-23 23:40:37 +0530

    added reversing linkedlist

--- JS_datastructures/LinkedList.js
+++ JS_datastructures/LinkedList.js
@@ -30,7 +30,7 @@ LinkedList.prototype.traverse = function(val) {
         start = start.next;
     }
     tstring += start.val;
-    console.log(tstring);
+    console.log(tstring)
 }
 
 LinkedList.prototype.search = function(elem) {
@@ -44,12 +44,45 @@ LinkedList.prototype.search = function(elem) {
     return -1
 }
 
+// Reverse a linked list
+LinkedList.prototype.reverse = function(start) {
+    if (start.next == null) {
+        console.log(start.val)
+        return;
+    } else {
+        this.reverse(start.next)
+        console.log(start.val)
+    }
+
+}
+
+// Check a linked list is palindrome or not
+LinkedList.prototype.isPalindrome = function() {
+    start = this.head
+    var temp = []
+    while (start.next !== null) {
+        temp.push(start.val)
+				start = start.next
+    }
+		temp.push(start.val)
+		console.log(temp)
+    for (var i = 0, j = temp.length - 1; i <= j; i++, j--) {
+        if (temp[i] !== temp[j]) {
+            return false
+        }
+    }
+    return true
+}
+
 llist = new LinkedList();
 console.log(llist.isEmpty())
 llist.push(2);
 llist.push(52);
-llist.push(13);
+llist.push(2);
 llist.traverse();
+
 console.log(llist.search(52))
 console.log(llist.search(12))
 console.log(llist.isEmpty())
+llist.reverse(llist.head)
+console.log(llist.isPalindrome())
